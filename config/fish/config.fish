a163e732d5d0456214b4c92321ce3f6ec0121938
Eugene Morozov <jmv@emorozov.net>
2020-05-26 09:27:08 +0300

    Added fisher, removed virtualfish.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,9 @@
-eval (python3 -m virtualfish auto_activation)
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 
 function reverse_history_search
   history | fzf --no-sort | read -l command
@@ -7,7 +12,15 @@ function reverse_history_search
   end
 end
 
+
 function fish_user_key_bindings
   bind \cr reverse_history_search
 end
+
+
+# bobthefish prompt configuration
+set -g theme_display_date no
+set -g theme_color_scheme solarized
+
+status --is-interactive; and source (pyenv init -|psub)
 
