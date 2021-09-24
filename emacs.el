2b64620970203538a90b8fa26b59647d4b3e9371
Eugene Morozov <jmv@emorozov.net>
2021-09-24 11:05:12 +0300

    Treat underscore as part of a word.

--- emacs.el
+++ emacs.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+(modify-syntax-entry ?_ "w" (standard-syntax-table))
+
 (setq evil-emacs-state-modes nil)
 (setq evil-insert-state-modes nil)
 (setq evil-motion-state-modes nil)
@@ -9,3 +11,16 @@
 (evil-mode 1)
 
 (set-face-attribute 'default nil :font "Iosevka Term" :height 120)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(gruvbox-theme evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'gruvbox t)
