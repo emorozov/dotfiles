1f2aea05146b87afc5eb673d2bb048065ec77dd1
Eugene Morozov <jmv@emorozov.net>
2022-08-09 07:36:18 +0300

    Adds comments and neogit plugins.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -5,9 +5,33 @@ return require('packer').startup(function()
   use 'joshdick/onedark.vim'
   use 'powerman/vim-plugin-ruscmd'
   use 'rcarriga/nvim-notify'
+
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = [[require('config.treesitter')]] }
+
   use { { 'nvim-orgmode/orgmode.nvim', config = [[require('config.orgmode')]] }, 'akinsho/org-bullets.nvim' }
+
   use 'neovim/nvim-lspconfig'
+
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end,
+  }
+
+  use {
+    'TimUntersberger/neogit',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup {
+        signs = {
+          section = { "﬌", "" },
+          item = { "﬌", "" },
+        }
+      }
+    end,
+  }
 
   if packer_bootstrap then
     require('packer').sync()
