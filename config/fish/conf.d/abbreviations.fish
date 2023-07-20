9dcd117380e0edd0bca2283b4a5391c40205831d
Eugene Morozov <jmv@emorozov.net>
2023-07-20 10:01:47 +0300

    Improves abbreviations.

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -1,10 +1,12 @@
-abbr --add gs git status -sb
-abbr --add gco git checkout
-abbr --add ga git add
-abbr --add gc git commit
-abbr --add gl git stash list
-abbr --add gp git stash show "'stash@{0}'"
-abbr --add de docker exec -it
-abbr --add n nvim
+abbr --add --position command gs git status -sb
+abbr --add --position command gco git checkout
+abbr --add --position command ga git add
+abbr --add --position command gc git commit
+abbr --add --position command gl git stash list
+abbr --add --position command gp git stash show "'stash@{0}'"
+abbr --add --position command --set-cursor gsi git stash push -m "'%'"
+abbr --add --position command gso git stash pop
+abbr --add --position command de docker exec -it
+abbr --add --position command n nvim
 abbr --add g grep
 
