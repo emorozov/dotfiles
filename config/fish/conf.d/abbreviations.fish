c908d1bb3f95931856e90d658703250e872494b1
Eugene Morozov <jmv@emorozov.net>
2023-07-31 23:24:37 +0300

    Adds search and edit abbreviation.

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -2,8 +2,9 @@ abbr --add --position command gs git status -sb
 abbr --add --position command gco git checkout
 abbr --add --position command ga git add
 abbr --add --position command gc git commit
-abbr --add --position command gl git stash list
-abbr --add --position command gp git stash show "'stash@{0}'"
+abbr --add --position command gp git push
+abbr --add --position command gsl git stash list
+abbr --add --position command gsp git stash show "'stash@{0}'"
 abbr --add --position command --set-cursor gsi git stash push -m "'%'"
 abbr --add --position command gso git stash pop
 abbr --add --position command de docker exec -it
@@ -12,5 +13,6 @@ abbr -a g grep
 abbr -a h hyperfine
 abbr -a --position command x xz -9ev -T0
 abbr -a --position command u xzcat -v
+abbr -a --position command e "nvim (rg -n --color=never . | fzf | awk -F: '{ print \$1, \"+\"\$2 }' | string split ' ')"
 abbr -a --position anywhere --set-cursor l "%| less"
 abbr -a --position anywhere te "&| tee"
