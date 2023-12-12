a860a9df4b5a54d4f57e47012b3a71a24a4ead37
Eugene Morozov <jmv@emorozov.net>
2023-12-12 11:38:52 +0300

    Use system clipboard in nvim/neovide

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -13,7 +13,8 @@ opt.selection = "exclusive"
 opt.ignorecase = true
 opt.smartcase = true
 
-vim.opt.listchars = "tab:➜-,space:·,trail:·,eol:¶"
+opt.listchars = "tab:➜-,space:·,trail:·,eol:¶"
+opt.clipboard = 'unnamedplus'
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
