8d7c47da9e74a6f044e2e84ac0699508b64fd10f
Eugene Morozov <jmv@emorozov.net>
2024-01-15 08:49:21 +0300

    Start search with commandline arguments.

--- config/fish/functions/,cdf.fish
+++ config/fish/functions/,cdf.fish
@@ -1,5 +1,5 @@
 function ,cdf
-    set dir (fd -t d --search-path ~ | fzf --preview="tree -L 1 {}" --bind=space:toggle-preview --preview-window=:hidden)
+    set dir (fd -t d --search-path ~ | fzf --query=$argv --preview="tree -L 1 {}" --bind=space:toggle-preview --preview-window=:hidden)
 
     if test -n "$dir"
         cd $dir
