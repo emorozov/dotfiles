920a9b9526b6afdee9b7fdcb2160f7abe91f6070
Eugene Morozov <jmv@emorozov.net>
2024-01-23 17:39:10 +0300

    Fixes python virtualenv activation.

--- config/nvim/lua/config/plugins/lsp/python.lua
+++ config/nvim/lua/config/plugins/lsp/python.lua
@@ -3,8 +3,10 @@ return {
 	dependencies = {
 		"neovim/nvim-lspconfig",
 		"nvim-telescope/telescope.nvim",
-		"mfussenegger/nvim-dap-python"
+		"mfussenegger/nvim-dap-python",
+		"rcarriga/nvim-notify",
 	},
+	lazy = true,
 	config = function(_, opts)
 		local venv_selector = require("venv-selector")
 
@@ -15,36 +17,19 @@ return {
 		venv_selector.setup(opts)
 	end,
 	init = function()
-		vim.api.nvim_create_autocmd("VimEnter", {
-			desc = "Auto select virtualenv Nvim open",
-			pattern = "*",
+		vim.api.nvim_create_autocmd("Filetype", {
+			pattern = { "python" },
 			callback = function()
-				local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-
-				if venv == "" then
-					local names = { "venv", ".venv", "env", ".env" }
-					for _, name in ipairs(names) do
-						if venv ~= "" then
-							break
-						end
-
-						venv = vim.fn.finddir(name, vim.fn.getcwd())
-					end
-				end
-
-				if venv ~= "" then
-					require("venv-selector").retrieve_from_cache()
-				end
+				require("venv-selector").retrieve_from_cache()
 			end,
-			once = true,
 		})
 
 		return true
 	end,
-  keys = {
-    { '<leader>vs', '<cmd>VenvSelect<cr>' },
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-  },
+	keys = {
+		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+	},
 	-- opts = {
 	--   dap_enabled = true,
 	-- },
