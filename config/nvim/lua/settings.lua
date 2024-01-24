ad21e7eb881d49da53e27865a16c2bffa22826b8
Eugene Morozov <jmv@emorozov.net>
2024-01-24 16:45:51 +0300

    Allows closing some types of buffers with q.

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -54,6 +54,25 @@ vim.api.nvim_create_autocmd("BufReadPre", {
 	end,
 })
 
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = {
+		"qf",
+		"help",
+		"man",
+		"notify",
+		"lspinfo",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"PlenaryTestPopup",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
+
 opt.termguicolors = true
 opt.guifont = { "JetBrainsMono NFP", ":h11" }
 
