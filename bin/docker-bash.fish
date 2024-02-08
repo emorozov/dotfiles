8534f73747115790d75dd32b792bf6ac21c307b9
Eugene Morozov <jmv@emorozov.net>
2024-02-08 23:40:09 +0300

    Fixes container selection

--- bin/docker-bash.fish
+++ bin/docker-bash.fish
@@ -3,8 +3,6 @@
 set CONTAINER (docker ps | tail -n +2 | rofi -dmenu -p 'Select Container' | cut -d ' ' -f 1)
 
 
-if not set -q CONTAINER
-  echo 'Cancel'
-else
-  rofi-sensible-terminal -e docker exec -ti "$CONTAINER" /bin/bash
+if test -n "$CONTAINER"
+    rofi-sensible-terminal -e docker exec -ti "$CONTAINER" /bin/bash
 end
