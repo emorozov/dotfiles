66e5987255009600420dc434248a4f42866df854
Eugene Morozov <jmv@emorozov.net>
2024-02-11 21:27:32 +0300

    Also implements Shift-Tab for completion.

--- config/nvim/lua/config/plugins/nvim-cmp.lua
+++ config/nvim/lua/config/plugins/nvim-cmp.lua
@@ -47,6 +47,13 @@ return {
 						fallback()
 					end
 				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
