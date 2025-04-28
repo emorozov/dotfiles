3c44c2352e1617bbc56ccc6e0a560cf87ca55e8a
Eugene Morozov <jmv@emorozov.net>
2025-04-28 14:26:44 +0300

    Leaves only useful autosnippets.

--- config/nvim/snippets/all.lua
+++ config/nvim/snippets/all.lua
@@ -2,52 +2,11 @@ local ls = require("luasnip")
 
 local s = ls.snippet
 local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
 
 return {
-	-- Autosnippets
-	---- Autopairs
-	s({ trig = "dq", dscr = "Double quotes" }, {
-		t('"'),
-		f(_G.LuaSnipConfig.visual_selection),
-		i(1),
-		t('"'),
-		i(0),
-	}),
-	s({ trig = "sq", dscr = "Single quotes" }, {
-		t("'"),
-		f(_G.LuaSnipConfig.visual_selection),
-		i(1),
-		t("'"),
-		i(0),
-	}),
-	s({ trig = "{{", wordTrig = false, dscr = "Braces" }, {
-		t("{"),
-		f(_G.LuaSnipConfig.visual_selection),
-		i(1),
-		t("}"),
-		i(0),
-	}),
-	s({ trig = "((", wordTrig = false, dscr = "Parenthesis" }, {
-		t("("),
-		f(_G.LuaSnipConfig.visual_selection),
-		i(1),
-		t(")"),
-		i(0),
-	}),
-	s({ trig = "[[", wordTrig = false, dscr = "Brackets" }, {
-		t("["),
-		f(_G.LuaSnipConfig.visual_selection),
-		i(1),
-		t("]"),
-		i(0),
-	}),
-	s({ trig = "<<", wordTrig = false, dscr = "<>" }, {
-		t("<"),
-		f(_G.LuaSnipConfig.visual_selection),
-		i(1),
-		t(">"),
-		i(0),
-	}),
+  -- Autosnippets
+  s(
+    {trig = 'cdate', desc = "Insert current date", snippetType = "autosnippet"},
+    {t(vim.fn.strftime("%Y-%m-%d"))}
+  ),
 }
