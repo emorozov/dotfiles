1117c135ae8914e39c05fce2a50d8074549ebe09
Eugene Morozov <jmv@emorozov.net>
2025-05-18 17:11:49 +0300

    Fixes keybindings conflict.

--- config/nvim/lua/config/plugins/lsp/toggle-diagnostics.lua
+++ config/nvim/lua/config/plugins/lsp/toggle-diagnostics.lua
@@ -8,6 +8,6 @@ return {
 		require("toggle_lsp_diagnostics").init()
 	end,
 	keys = {
-		{ "<leader>td", "<cmd>ToggleDiag<cr>", desc = "Toggle LSP Diagnostics" },
+		{ "<leader>tD", "<cmd>ToggleDiag<cr>", desc = "Toggle LSP Diagnostics" },
 	},
 }
