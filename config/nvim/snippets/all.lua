c1c4a7a960f3d1cb44896af9950c079c9313b03d
Eugene Morozov <jmv@emorozov.net>
2025-07-02 12:51:03 +0300

    Insert the actual current date, not the date when nvim was started.

--- config/nvim/snippets/all.lua
+++ config/nvim/snippets/all.lua
@@ -1,12 +1,14 @@
 local ls = require("luasnip")
 
 local s = ls.snippet
-local t = ls.text_node
+local f = ls.function_node
 
 return {
   -- Autosnippets
   s(
     {trig = 'cdate', desc = "Insert current date", snippetType = "autosnippet"},
-    {t(vim.fn.strftime("%Y-%m-%d"))}
+    {f(function()
+      return vim.fn.strftime("%Y-%m-%d")
+    end)}
   ),
 }
